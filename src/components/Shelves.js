c5c68f58b7bf13ce7cdf1392bb6c5599b9a8cd01
unknwnlistener <10276779+unknwnlistener@users.noreply.github.com>
2021-04-16 14:36:43 +0100

    [UPDATE] Modified the lists and contents of Education and Experience

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -12,6 +12,22 @@ export const Shelves = () => {
           developer
         </p>
       </div>
+      <div className="section gold-bordered experience">
+        <h1>Experience</h1>
+        <ContentDescription
+          collegeName="Media.net"
+          year="July 2017 - August 2019"
+          place="Mumbai, India"
+          degree="Web Application Developer"
+          desc={[
+            "Perform Media (www.performmedia.com): Company to Publisher advertising platform connecting US clients Yahoo/MSN, Forbes, Reuters to Advertisers",
+            "Improved client interactions by saving time spent by 40%, through the development for excel and CSV file import feature in parsing Ad Campaign information for storage in system.",
+            "Reduced collaborative errors for the front-end team by 20% and updated Continuous Integration(CI) by redesigning version control workflow from Apache SVN to Git workflow.",
+            "Enhanced personal knowledge base for Digital Marketing through development for Search Engine Optimization(SEO), Google AdWords and AdSense.",
+            "Leveraged knowledge in SVN, Git, Agile development, programmed in HTML/CSS/JS, Angular and React using VS Code, C# using ASP.Net MVC in Visual Studio(VS) 2015.",
+          ]}
+        ></ContentDescription>
+      </div>
       <div className="section gold-bordered education">
         <h1>Education</h1>
         <ContentDescription
@@ -35,22 +51,6 @@ export const Shelves = () => {
             "Project and paper published on “Ad-Hoc Network for File Sharing”.",
             "Web based HTML5 game creation and live hosting for over 500 students across the country.",
             "Organized multiple open workshops on Web Development (HTML, CSS, PHP), UI/UX Design (Photoshop) and Version Control (Git) teaching an average of 40 students.",
-          ]}
-        ></ContentDescription>
-      </div>
-      <div className="section gold-bordered experience">
-        <h1>Experience</h1>
-        <ContentDescription
-          collegeName="Media.net"
-          year="July 2017 - August 2019"
-          place="Mumbai, India"
-          degree="Web Application Developer"
-          desc={[
-            "Perform Media (www.performmedia.com): Company to Publisher advertising platform connecting US clients Yahoo/MSN, Forbes, Reuters to Advertisers",
-            "Improved client interactions by saving time spent by 40%, through the development for excel and CSV file import feature in parsing Ad Campaign information for storage in system.",
-            "Reduced collaborative errors for the front-end team by 20% and updated Continuous Integration(CI) by redesigning version control workflow from Apache SVN to Git workflow.",
-            "Enhanced personal knowledge base for Digital Marketing through development for Search Engine Optimization(SEO), Google AdWords and AdSense.",
-            "Leveraged knowledge in SVN, Git, Agile development, programmed in HTML/CSS/JS, Angular and React using VS Code, C# using ASP.Net MVC in Visual Studio(VS) 2015.",
           ]}
         ></ContentDescription>
       </div>
