6f97da5981bd005d0742853c8e00762c43cb6c33
unknwnlistener <10276779+unknwnlistener@users.noreply.github.com>
2021-05-03 22:42:51 +0100

    Trial border stylings

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,11 +3,18 @@ import "../styles/nav.css";
 import self from "../assets/self.png";
 
 export const SideNav = ({ tabList, currentTab, setTab }) => {
-  let activateTab = (tab) => {
+  let activateTab = (tab, e) => {
+    console.log(e);
     if (tab === currentTab) return;
     else {
       setTab(tab);
     }
+  };
+
+  let getClassesButton = (i) => {
+    let classList = ["item-list__button", "mono-font"];
+    if (currentTab === tabList[i]) classList.unshift("active-button");
+    return classList.join(" ");
   };
   return (
     <div className="nav-container">
@@ -18,25 +25,25 @@ export const SideNav = ({ tabList, currentTab, setTab }) => {
         <ul className="item-list">
           <button
             onClick={() => activateTab(tabList[0])}
-            className="item-list__button"
+            className={getClassesButton(0)}
           >
             Who am I?
           </button>
           <button
             onClick={() => activateTab(tabList[1])}
-            className="item-list__button"
+            className={getClassesButton(1)}
           >
             What I've done
           </button>
           <button
             onClick={() => activateTab(tabList[2])}
-            className="item-list__button"
+            className={getClassesButton(2)}
           >
             What I'm doing
           </button>
           <button
             onClick={() => activateTab(tabList[3])}
-            className="item-list__button"
+            className={getClassesButton(3)}
           >
             Want to say hi?
           </button>
