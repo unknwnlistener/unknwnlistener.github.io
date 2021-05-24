ac6b29d70dfa26ec1530122e3fdeedf5b30fb76b
unknwnlistener <10276779+unknwnlistener@users.noreply.github.com>
2021-05-24 18:49:39 +0100

    :art: Added title and links to projects

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-export const ImageContainer = ({ source, alt, desc }) => {
+export const ImageContainer = ({ source, alt, desc, link, title, github }) => {
   return (
-    <div class="image-container">
-      <img src={source} alt={alt} />
-      <span class="image__desc">{desc}</span>
+    <div className="project-container">
+      <div class="image-container">
+        <img src={source} alt={alt} />
+        <span class="image__desc">{desc}</span>
+      </div>
+
+      <h3 className="project__title">{title}</h3>
+      <div className="project__links">
+        <p>
+          <a href={link} target="_blank" rel="noreferrer">
+            Site
+          </a>
+        </p>
+        <p>
+          <a href={github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,16 +15,25 @@ export const Projects = () => {
       image: waImg,
       altText: "screenshot of Whatsapp Conversation Reader",
       desc: "Web Application to read exported whatsapp conversation in a friendly format",
+      link: "https://bee-wa-reader.netlify.app/",
+      title: "WhatsApp Chat Reader",
+      github: "https://github.com/unknwnlistener/whatsapp-reader",
     },
     {
       image: webImg,
-      altText: "screenshot of Personal Website",
+      altText: "screenshot of Portfolio Website",
       desc: "This website you're on right now! To showcase and summarize who I am and what I've done",
+      link: "#",
+      title: "Portfolio Website",
+      github: "https://github.com/unknwnlistener/unknwnlistener.github.io",
     },
     {
       image: chuckImg,
       altText: "screenshot of Chuck Norris Joke Generator",
       desc: "One of the first React projects I took up with Florin Pop's community to learn React.js",
+      link: "https://chuck-norris-joker.netlify.app/",
+      title: "Chuck Norris Joke Generator",
+      github: "https://github.com/unknwnlistener/chuck-norris-joke-generator",
     },
   ];
   return (
@@ -37,6 +46,9 @@ export const Projects = () => {
               source={proj.image}
               alt={proj.altText}
               desc={proj.desc}
+              link={proj.link}
+              title={proj.title}
+              github={proj.github}
             ></ImageContainer>
           ))}
         </section>
